0e4d788d5ff790c0a55ea525f89cdbfaa929a1a6
sungurov <sungurov@diakont.com>
2026-03-17 19:41:58 +0300

    Отладил генерацию step

--- src/main.cpp
+++ src/main.cpp
@@ -2,161 +2,182 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
-#include "driver/rmt_tx.h"
-#include "StepMotor/StepGenerator.h"
-
-#include "Helpers/LedController.h"
-
 #include "led_strip.h"
+#include "driver/mcpwm_prelude.h"
 
-///////////////////////////////Change the following configurations according to your board//////////////////////////////
-const gpio_num_t STEP_MOTOR_GPIO_EN = GPIO_NUM_15;
-const gpio_num_t STEP_MOTOR_GPIO_DIR = GPIO_NUM_2;
-const gpio_num_t STEP_MOTOR_GPIO_STEP = GPIO_NUM_4;
-#define STEP_MOTOR_ENABLE_LEVEL  0 // DRV8825 is enabled on low level
-#define STEP_MOTOR_SPIN_DIR_CLOCKWISE 0
-#define STEP_MOTOR_SPIN_DIR_COUNTERCLOCKWISE !STEP_MOTOR_SPIN_DIR_CLOCKWISE
-
-#define STEP_MOTOR_RESOLUTION_HZ 1000000 // 1MHz resolution
-
-static const char *TAG = "example";
+#include <cmath>
 
 
+const gpio_num_t GPIO_EN = GPIO_NUM_15;
+const gpio_num_t GPIO_DIR = GPIO_NUM_2;
+const gpio_num_t GPIO_STEP = GPIO_NUM_4;
+
+
+const uint32_t timerResolution = 1'000'000; // 1МГц, 1/частота = 1 мкс точность
+
+// --- Глобальные объекты драйвера ---
+static mcpwm_timer_handle_t stepper_timer = nullptr;
+static mcpwm_oper_handle_t stepper_oper = nullptr;
+static mcpwm_cmpr_handle_t stepper_cmp = nullptr;
+static mcpwm_gen_handle_t stepper_gen = nullptr;
+
+void stepper_motor_init()
+{
+    const uint32_t defaultTimerPeriod = 1000;
+
+    // 1. Настройка ENABLE (обычный GPIO)
+    gpio_config_t io_conf_1 = {
+        .pin_bit_mask = (1ULL << GPIO_EN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf_1);
+    gpio_set_level(GPIO_DIR, 0);
+
+    // 1. Настройка DIR (обычный GPIO)
+    gpio_config_t io_conf_2 = {
+        .pin_bit_mask = (1ULL << GPIO_DIR),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf_2);
+    gpio_set_level(GPIO_DIR, 1); // Направление вперед
+
+    // 2. Создание таймера (Сердце системы)
+    mcpwm_timer_config_t timer_config = {
+        .group_id = 0,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = timerResolution,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+        .period_ticks = defaultTimerPeriod,
+        .intr_priority = 0,
+        .flags = {},
+    };
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &stepper_timer));
+
+    // 3. Создание оператора (привязка к таймеру)
+    mcpwm_operator_config_t oper_config = {
+        .group_id = 0,
+        .intr_priority = 0,
+        .flags = {},
+    };
+    ESP_ERROR_CHECK(mcpwm_new_operator(&oper_config, &stepper_oper));
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(stepper_oper, stepper_timer));
+
+    // 4. Создание компаратора
+    mcpwm_comparator_config_t cmp_config = {
+        .intr_priority = 0,
+        .flags = {
+            .update_cmp_on_tez = true,   // Обновлять при счете = 0
+            .update_cmp_on_tep = false,
+            .update_cmp_on_sync = false,
+        },
+    };
+    ESP_ERROR_CHECK(mcpwm_new_comparator(stepper_oper, &cmp_config, &stepper_cmp));
+
+    // 5. Создание генератора (Выход сигнала)
+    mcpwm_generator_config_t gen_config = {
+        .gen_gpio_num = GPIO_STEP,
+        .flags = {},
+    };
+    ESP_ERROR_CHECK(mcpwm_new_generator(stepper_oper, &gen_config, &stepper_gen));
+
+    // 6. Настройка логики генератора
+    // Создаем импульс: низкий уровень в начале периода, высокий по достижении значения компаратора
+    mcpwm_generator_set_action_on_timer_event(stepper_gen, MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_LOW));
+    mcpwm_generator_set_action_on_compare_event(stepper_gen, MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, stepper_cmp, MCPWM_GEN_ACTION_HIGH));
+
+    // 7. Установка скважности (50% от периода)
+    mcpwm_comparator_set_compare_value(stepper_cmp, std::round(defaultTimerPeriod / 2.));
+
+    // 8. Запуск таймера
+    mcpwm_timer_enable(stepper_timer);
+    //mcpwm_timer_start_stop(stepper_timer, MCPWM_TIMER_START_NO_STOP);
+}
+
+// --- Управление скоростью ---
+void stepper_set_speed(float steps_per_sec)
+{
+    if (!stepper_timer)
+        return;
+
+    const uint32_t period_ticks = std::round(timerResolution / steps_per_sec);
+    const uint32_t compareVal = std::round(period_ticks / 2.);
+
+    // Меняем период
+    mcpwm_timer_set_period(stepper_timer, period_ticks);
+
+    // Меняем скважность
+    mcpwm_comparator_set_compare_value(stepper_cmp, compareVal);
+}
+
+// --- Старт / Стоп ---
+void stepper_start()
+{
+    mcpwm_timer_start_stop(stepper_timer, MCPWM_TIMER_START_NO_STOP);
+}
+
+void stepper_stop()
+{
+    mcpwm_timer_start_stop(stepper_timer, MCPWM_TIMER_STOP_FULL);
+}
 
 extern "C" void app_main()
 {
-    /*gpio_set_direction(STEP_MOTOR_GPIO_EN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(STEP_MOTOR_GPIO_DIR, GPIO_MODE_OUTPUT);
-    gpio_set_direction(STEP_MOTOR_GPIO_STEP, GPIO_MODE_OUTPUT);
+    // // RGB Светодиод
+    // led_strip_handle_t led_strip;
+    // /* Конфигурация ленты (общая) */
+    // led_strip_config_t strip_config = {
+    //     .strip_gpio_num = 48,
+    //     .max_leds = 1,
+    //     .led_model = LED_MODEL_WS2812,
+    //     .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB,
+    //     .flags = {},
+    // };
+    // /* Конфигурация RMT (аппаратный контроллер) */
+    // led_strip_rmt_config_t rmt_config = {
+    //     .clk_src = RMT_CLK_SRC_DEFAULT,
+    //     .resolution_hz = 10000000, // 10MHz
+    //     .mem_block_symbols = 64,
+    //     .flags = {},
+    // };
+    // /* Создание устройства */
+    // ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    // /* Очистка ленты перед началом (выключение) */
+    // ESP_ERROR_CHECK(led_strip_clear(led_strip));
 
-    gpio_set_level(STEP_MOTOR_GPIO_EN, 0);
-    gpio_set_level(STEP_MOTOR_GPIO_DIR, 0);
-    gpio_set_level(STEP_MOTOR_GPIO_STEP, 0);
+    // uint16_t h = 0;
+    // uint8_t s = 255;
+    // uint8_t v = 10;
+
+    // while (1)
+    // {
+    //     led_strip_set_pixel_hsv(led_strip, 0, h, s, v);
+    //     led_strip_refresh(led_strip);
+    //     vTaskDelay(pdMS_TO_TICKS(10));
+    //     ++h;
+    //     if (h > 360)
+    //         h = 0;
+    // }
+
+    float speed = 100;
+    stepper_motor_init();
+    stepper_set_speed(speed);
+    mcpwm_timer_start_stop(stepper_timer, MCPWM_TIMER_START_NO_STOP);
+
+    for (int i = 0; i < 30; ++i)
+    {
+        vTaskDelay(pdMS_TO_TICKS(500));
+        speed += 100;
+        stepper_set_speed(speed);
+    }
 
     while (1)
     {
-        gpio_set_level(STEP_MOTOR_GPIO_STEP, 1);
-        esp_rom_delay_us(20);
-        gpio_set_level(STEP_MOTOR_GPIO_STEP, 0);
-
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }*/
-
-    ESP_LOGI(TAG, "Initialize EN + DIR GPIO");
-    //const uint64_t bitMask = 0;
-    gpio_config_t en_dir_gpio_config = {
-        .pin_bit_mask = 1ULL << STEP_MOTOR_GPIO_DIR | 1ULL << STEP_MOTOR_GPIO_EN,
-        .mode = GPIO_MODE_OUTPUT,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
-    ESP_ERROR_CHECK(gpio_config(&en_dir_gpio_config));
-
-    ESP_LOGI(TAG, "Create RMT TX channel");
-    rmt_channel_handle_t motor_chan = NULL;
-    rmt_tx_channel_config_t tx_chan_config = {
-        .gpio_num = STEP_MOTOR_GPIO_STEP,
-        .clk_src = RMT_CLK_SRC_DEFAULT, // select clock source
-        .resolution_hz = STEP_MOTOR_RESOLUTION_HZ,
-        .mem_block_symbols = 64,
-        .trans_queue_depth = 10, // set the number of transactions that can be pending in the background
-    };
-    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &motor_chan));
-
-    ESP_LOGI(TAG, "Set spin direction");
-    gpio_set_level(STEP_MOTOR_GPIO_DIR, STEP_MOTOR_SPIN_DIR_CLOCKWISE);
-    ESP_LOGI(TAG, "Enable step motor");
-    gpio_set_level(STEP_MOTOR_GPIO_EN, STEP_MOTOR_ENABLE_LEVEL);
-
-    ESP_LOGI(TAG, "Create motor encoders");
-    stepper_motor_curve_encoder_config_t accel_encoder_config = {
-        .resolution = STEP_MOTOR_RESOLUTION_HZ,
-        .sample_points = 500,
-        .start_freq_hz = 500,
-        .end_freq_hz = 1500,
-    };
-    rmt_encoder_handle_t accel_motor_encoder = NULL;
-    ESP_ERROR_CHECK(rmt_new_stepper_motor_curve_encoder(&accel_encoder_config, &accel_motor_encoder));
-
-    stepper_motor_uniform_encoder_config_t uniform_encoder_config = {
-        .resolution = STEP_MOTOR_RESOLUTION_HZ,
-    };
-    rmt_encoder_handle_t uniform_motor_encoder = NULL;
-    ESP_ERROR_CHECK(rmt_new_stepper_motor_uniform_encoder(&uniform_encoder_config, &uniform_motor_encoder));
-
-    stepper_motor_curve_encoder_config_t decel_encoder_config = {
-        .resolution = STEP_MOTOR_RESOLUTION_HZ,
-        .sample_points = 500,
-        .start_freq_hz = 1500,
-        .end_freq_hz = 500,
-    };
-    rmt_encoder_handle_t decel_motor_encoder = NULL;
-    ESP_ERROR_CHECK(rmt_new_stepper_motor_curve_encoder(&decel_encoder_config, &decel_motor_encoder));
-
-    ESP_LOGI(TAG, "Enable RMT channel");
-    ESP_ERROR_CHECK(rmt_enable(motor_chan));
-
-    ESP_LOGI(TAG, "Spin motor for 6000 steps: 500 accel + 5000 uniform + 500 decel");
-    rmt_transmit_config_t tx_config = {
-        .loop_count = 0,
-    };
-
-    const static uint32_t accel_samples = 1000;
-    const static uint32_t uniform_speed_hz = 1500;
-    const static uint32_t decel_samples = 1000;
-
-
-
-    // Светодиод
-
-    // Handle светодиодной ленты
-    led_strip_handle_t led_strip;
-
-    /* Конфигурация ленты (общая) */
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = 48,
-        .max_leds = 1,
-        .led_model = LED_MODEL_WS2812
-    };
-
-    /* Конфигурация RMT (аппаратный контроллер) */
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10000000, // 10MHz
-    };
-
-    /* Создание устройства */
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-
-    /* Очистка ленты перед началом (выключение) */
-    ESP_ERROR_CHECK(led_strip_clear(led_strip));
-
-
-    uint16_t h = 0;
-    uint8_t s = 255;
-    uint8_t v = 10;
-
-    while (1)
-    {
-        /*// acceleration phase
-        tx_config.loop_count = 0;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, accel_motor_encoder, &accel_samples, sizeof(accel_samples), &tx_config));
-
-        // uniform phase
-        tx_config.loop_count = 10;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, uniform_motor_encoder, &uniform_speed_hz, sizeof(uniform_speed_hz), &tx_config));
-
-        // deceleration phase
-        tx_config.loop_count = 0;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, decel_motor_encoder, &decel_samples, sizeof(decel_samples), &tx_config));
-        // wait all transactions finished
-        ESP_ERROR_CHECK(rmt_tx_wait_all_done(motor_chan, -1));*/
-
-
-        /*led_strip_set_pixel_hsv(led_strip, 0, h, s, v);
-        led_strip_refresh(led_strip);
-        vTaskDelay(pdMS_TO_TICKS(10));
-
-        ++h;
-        if (h > 360)
-            h = 0;*/
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
